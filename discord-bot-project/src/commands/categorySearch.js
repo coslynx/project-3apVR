fc2ce9f361cf4dea159ca1828e6a45b4ccd98f2d
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-18 11:02:06 +0530

    Added: `discord-bot-project/src/commands/categorySearch.js`

---
+++ discord-bot-project/src/commands/categorySearch.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'categorysearch',
+    description: 'Search for lesbian pornographic content by category',
+    execute(message, args) {
+        // Logic to search for content by category
+    }
+};
